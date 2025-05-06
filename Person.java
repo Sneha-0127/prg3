179d7b6a23bab125900b3f23201703d39b27c588
Sneha-0127 <snehamk27112005@gmail.com>
2025-05-06 10:42:21 +0530

    person class

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    String name;
+    String dob;
+    public Person(String name, String dob) {
+        this.name = name;
+        this.dob = dob;
+    }
+    
+    public void displayPerson(){
+        System.out.println("Name: " + name);
+        System.out.println("date of birth: " + dob);
+    }
+}
